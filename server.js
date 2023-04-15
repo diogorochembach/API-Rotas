31587253cc64b33cebb1974ca1a298b259394a4d
diogorochembach <diogo.iju@gmail.com>
2023-04-15 12:24:39 -0300

    Criação API

---
+++ server.js
@@ -0,0 +1,8 @@
+import http from "http";
+import app from "./src/app.js";
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Servidor ouvindo em http://localhost:${port}`);
+});
---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const livros = [
+  { id: 1, titulo: "Mestre dos Magos" },
+  { id: 2, titulo: "O Hobbit" },
+];
+
+app.get("/", (req, res) => {
+  res.status(200).send("API Node iniciada");
+});
+
+app.get("/livros", (req, res) => {
+  res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).send("Livros cadastrados com sucesso");
+});
+
+export default app;
